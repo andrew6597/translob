2dbe7ca7ff286811ebb452ba77cd3ca71ae7d5df
Andreas Marinis <116451022+andrew6597@users.noreply.github.com>
2024-02-10 13:08:41 +0200

    Update main_tensorflow.py

--- TensorFLow_Version/main_tensorflow.py
+++ TensorFLow_Version/main_tensorflow.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     }
 
     # Fit the model
-    r = model.fit(ds_train, epochs=epochs, batch_size=batch_size, validation_data=ds_val, class_weights= train_class_weights)
+    r = model.fit(ds_train, epochs=epochs, batch_size=batch_size, validation_data=ds_val, class_weight= train_class_weights)
 
 
     # Finally test the model on test data
