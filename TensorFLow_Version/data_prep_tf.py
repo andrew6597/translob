e54b8e4bd74237d3fa2f49a81db3a60feb0e1a7d
Andreas Marinis <116451022+andrew6597@users.noreply.github.com>
2024-02-10 11:34:26 +0200

    Add files via upload

---
+++ TensorFLow_Version/data_prep_tf.py
@@ -0,0 +1,142 @@
+import tensorflow as tf
+
+
+def get_mid_price(x):
+   return (x[0] + x[2])/ 2.0
+
+def generate_labels(current_mid_price, future_mid_prices, alpha = 0.0005):
+
+    mean_future_mid_prices = tf.reduce_mean(future_mid_prices)
+
+    if current_mid_price >= 0:
+        condition_up = tf.greater(mean_future_mid_prices, current_mid_price + alpha * current_mid_price)
+        condition_down = tf.less(mean_future_mid_prices, current_mid_price - alpha * current_mid_price)
+    else:
+        condition_up = tf.greater(mean_future_mid_prices, current_mid_price - alpha * current_mid_price)
+        condition_down = tf.less(mean_future_mid_prices, current_mid_price + alpha * current_mid_price)
+
+    return tf.where(condition_up, 2, tf.where(condition_down, 0, 1))
+
+
+def generate_labels_no_mid(current_mid_price, future_mid_prices, alpha = 0.01):
+    future_mid = future_mid_prices[-1]
+    # Due to scaling mid prices can either be positive or negative
+    if current_mid_price >= 0:
+        condition_up = tf.greater(future_mid, current_mid_price + alpha * current_mid_price)
+        condition_down = tf.less(future_mid, current_mid_price - alpha * current_mid_price)
+    else:
+        condition_up = tf.greater(future_mid, current_mid_price - alpha * current_mid_price)
+        condition_down = tf.less(future_mid, current_mid_price + alpha * current_mid_price)
+
+    return tf.where(condition_up, 2, tf.where(condition_down, 0, 1))
+
+def generate_labels_no_scale(current_mid_price, future_mid_prices):
+    alpha = 0.0001
+    current_mid_price = current_mid_price
+    future_mid = future_mid_prices[-1]
+    condition_up = tf.greater(future_mid, current_mid_price + alpha * current_mid_price)
+    condition_down = tf.less(future_mid, current_mid_price - alpha * current_mid_price)
+
+    return tf.where(condition_up, 2, tf.where(condition_down, 0, 1))
+
+
+
+def make_window_dataset(ds, labels, window_size=100, shift=1, horizon=600):
+    # No split the data to time windows and get rid off the last window size and horizon +2(The +2 is for indexing purposes)
+    ds_windows = ds.window(window_size, shift=shift, stride=1, drop_remainder=True)
+    # ds_windows = ds_windows.take(ds_windows.cardinality().numpy() - (window_size - 1) - (horizon - 1))
+    label_windows = labels.window(window_size, shift=shift, stride=1, drop_remainder=True)
+
+    def sub_to_batch(sub):
+        return sub.batch(window_size, drop_remainder=True)
+
+    # First flatten the dataset to 1 dataset, then map it back with batches of size window_size. Now it is iterable as numpy
+    ds_windows = ds_windows.flat_map(sub_to_batch)
+
+    label_windows = label_windows.flat_map(sub_to_batch)
+
+    # Now we want to keep only the label from the last lob of every window
+    labels = label_windows.map(lambda x: x[-1])
+
+    combined_dataset = tf.data.Dataset.zip((ds_windows, labels))
+    return combined_dataset
+
+
+def train_data_pipe(tf_dataset, window_size, batch_size, k):
+    mid_prices = tf_dataset.map(get_mid_price, num_parallel_calls=tf.data.AUTOTUNE)
+    # Create future windows with size the wanted predicted horizon k
+    future_mid_prices = mid_prices.window(size=k, shift=1, drop_remainder=True)
+    # Flatmap them and make them numpy iterable
+    future_mid_prices = future_mid_prices.flat_map(lambda x: x.batch(k))
+
+    mid_prices = mid_prices.take(len(mid_prices) - k + 1)
+    # Now we can create the tensorflow dataset type that consists the label for every timestamp
+    tf_labels = tf.data.Dataset.zip((mid_prices, future_mid_prices)).map(
+        lambda current, future: (generate_labels(current, future)), num_parallel_calls=tf.data.AUTOTUNE)
+
+    ds = make_window_dataset(tf_dataset, tf_labels, window_size=window_size, shift=1, horizon=k)
+
+    ds = ds.cache()
+
+    neutral = 0
+    up = 0
+    down = 0
+    for data, label in ds:
+        if label.numpy() == 1:
+            neutral += 1
+        elif label.numpy() ==2:
+            up += 1
+        else:
+            down += 1
+
+    print('Stats of Training Dataset:')
+    print(f'{k/10} seconds horizon:')
+    print('-------------------')
+    print(f'Price went up {(up/(up+down+neutral))*100} %')
+    print(f'Price went down {(down/(up+down+neutral))*100} %')
+    print(f'Price stayed neutral {(neutral/(up+down+neutral))*100} %')
+
+    ds = ds.shuffle(up+down+neutral)
+
+    ds = ds.batch(batch_size=batch_size)
+
+    ds = ds.prefetch(tf.data.AUTOTUNE)
+
+    return ds
+
+
+def test_data_pipe(tf_dataset, window_size, batch_size, k):
+    mid_prices = tf_dataset.map(get_mid_price, num_parallel_calls=tf.data.AUTOTUNE)
+    # Create future windows with size the wanted predicted horizon k
+    future_mid_prices = mid_prices.window(size=k, shift=1, drop_remainder=True)
+    # This should be length: len(mid_prices) - (k  - 1)
+    # Flatmap them and make them numpy iterable iterable
+    future_mid_prices = future_mid_prices.flat_map(lambda x: x.batch(k))
+
+    # Now we can create the tensorflow dataset type that consists the label for every timestamp
+    tf_labels = tf.data.Dataset.zip((mid_prices, future_mid_prices)).map(
+        lambda current, future: (generate_labels(current, future)), num_parallel_calls=tf.data.AUTOTUNE)
+
+    ds = make_window_dataset(tf_dataset, tf_labels, window_size=window_size, shift=1, horizon=k)
+
+    neutral = 0
+    up = 0
+    down = 0
+    for data, label in ds:
+        if label.numpy() == 1:
+            neutral += 1
+        elif label.numpy() ==2:
+            up += 1
+        else:
+            down += 1
+
+    print('Stats of Val/Test Dataset')
+    print(f'{k/10} seconds horizon:')
+    print('-------------------')
+    print(f'Price went up {(up/(up+down+neutral))*100} %')
+    print(f'Price went down {(down/(up+down+neutral))*100} %')
+    print(f'Price stayed neutral {(neutral/(up+down+neutral))*100} %')
+    ds = ds.batch(batch_size=batch_size)
+    ds = ds.cache()
+
+    return ds
---
+++ TensorFLow_Version/main_tensorflow.py
@@ -0,0 +1,103 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import matplotlib.pyplot as plt
+import seaborn as sn
+from sklearn.metrics import confusion_matrix
+from data_prep_tf import train_data_pipe,test_data_pipe
+from model_builider_tf import TransLOB
+import pandas as pd
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+import numpy as np
+
+if __name__ == '__main__':
+    print('Started running...')
+    # Set params
+    n_dim = 40
+    # first_train = 1495000
+    # print(f'Dataset starts at time {first_train}')
+    # last_train = first_train + 150000
+    # We will keep 20,000 rows for validation and the next 20,000 for test
+    # first_test = last_train + 20000
+    # last_test = first_test + 20000
+
+    # Load and split df
+    df = pd.read_csv('/content/drive/My Drive/LOBseries_100ms.csv').drop(columns=['Unnamed: 0'])
+    print('Loaded df', df.shape)
+
+    # df_train = df.iloc[first_train:last_train, :n_dim]
+    # df_val = df.iloc[last_train:first_test, :n_dim]
+    # df_test = df.iloc[first_test: last_test, :n_dim]
+    val_point = int(len(df) * 2 / 3)
+    test_point = val_point + 150000
+
+    df_train = df.iloc[:val_point, :n_dim]
+    df_val = df.iloc[val_point:test_point, :n_dim]
+    df_test = df.iloc[test_point:, :n_dim]
+
+    # Scale the data
+    scaler = StandardScaler()
+    scaler.fit(df_train)
+    df_train_scaled = pd.DataFrame(scaler.transform(df_train))
+    df_val_scaled = pd.DataFrame(scaler.transform(df_val))
+    df_test_scaled = pd.DataFrame(scaler.transform(df_test))
+
+    # Load the data as tensorflow datasets for efficiency
+    tf_dataset_train = tf.data.Dataset.from_tensor_slices(df_train_scaled)
+    tf_dataset_val = tf.data.Dataset.from_tensor_slices(df_val_scaled)
+    tf_dataset_test = tf.data.Dataset.from_tensor_slices(df_test_scaled)
+
+    # Set params
+    window_size = 100
+    batch_size = 32
+    k = 100
+    epochs = 2
+    lr = 0.0001
+
+    # Set the data pipeline for preprocessing before training
+    ds_train = train_data_pipe(tf_dataset_train, window_size, batch_size, k)
+    ds_val = test_data_pipe(tf_dataset_val, window_size, batch_size, k)
+    ds_test = test_data_pipe(tf_dataset_test, window_size, batch_size, k)
+	
+    # Create and compile the model
+    model = TransLOB(window_size,n_dim)
+
+    model.compile(
+        tf.keras.optimizers.Adam(
+            learning_rate=lr,
+            beta_1=0.9,
+            beta_2=0.999,
+            name="Adam",
+        ),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+    # Fit the model
+    r = model.fit(ds_train, epochs=epochs, batch_size=batch_size, validation_data=ds_val)
+
+    plt.plot(r.history['loss'], label ='loss')
+    plt.plot(r.history['val_loss'], label = 'val_loss')
+    plt.legend()
+    plt.show()
+
+    # Finally test the model on test data
+    predictions = model.predict(ds_test)
+    predicted_classes = np.argmax(predictions, axis=1)
+    true_classes = []
+
+    for _, labels in ds_test:
+        for label in labels:
+            true_classes.append(label.numpy())
+    true_classes = np.array(true_classes)
+    accuracy = np.mean(predicted_classes == true_classes)
+    print(f'Test Accuracy: {accuracy * 100:.2f}%')
+    
+    cm = confusion_matrix(true_classes, predicted_classes)
+    plt.figure(figsize=(10, 8))
+    sn.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Down','Neutral','Up'], yticklabels=['Down','Neutral','Up'])
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title('Confusion Matrix')
+    plt.show()
+    
