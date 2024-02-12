19b91e8cb0eab29ba371082c3a7b831de0183350
Andreas Marinis <116451022+andrew6597@users.noreply.github.com>
2024-02-12 17:26:27 +0200

    Update data_prep_tf.py

--- TensorFLow_Version/data_prep_tf.py
+++ TensorFLow_Version/data_prep_tf.py
@@ -3,8 +3,11 @@ import numpy as np
 
 def z_score(df):
     means = df.expanding().mean().shift()  # Calculate expanding mean and shift by one
+    print(means)
     sums = df.expanding().sum().shift()
+    print(sums)
     stds = (sums - means)/np.array([df.index.values] * 40).T
+    print(stds)
     normalized_df = (df.iloc[1:,:] - means.iloc[1:,:]) / stds.iloc[1:,:]
     return normalized_df.reset_index(drop = True, inplace = True)
 
